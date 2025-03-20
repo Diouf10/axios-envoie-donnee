24f06b07ea2f9fe4078fcf79daa77687f4ea34c8
mouhammadwaganediouf <dioufmouhammad@gmail.com>
2025-03-20 12:37:45 -0400

    axios post fini

---
+++ script.js
@@ -0,0 +1,71 @@
+const url = 'https://lesoublisdelinfo.com/api.php';
+
+// let requete = new XMLHttpRequest();
+
+// Get
+// requete.open('GET', url);
+// requete.responseType = 'json';
+// requete.send();
+
+// Post
+// requete.open('POST', url);
+// requete.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// requete.responseType = 'json';
+// requete.send('prenom=John');
+
+// requete.onload = function() {
+//   if (requete.readyState === XMLHttpRequest.DONE) {
+//     if (requete.status === 200) {
+//       let reponse = requete.response;
+//       console.log(reponse);
+//     }
+//     else {
+//       alert('Un problème est intervenu, merci de revenir plus tard.');
+//     }
+//   }
+// }
+
+// async function Effectuer(prenom) {
+//     const requete = await fetch(url, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded'
+//           // si on veut envoyer du JSON
+//           // 'Content-Type': 'application/json'
+//         },
+//         body: new URLSearchParams({
+//           prenom: prenom
+//         })
+
+//         // si on veut envoyer du JSON
+//         // body: JSON.stringify({
+//         //   prenom: prenom
+//         // })
+//       });
+
+//       if (!requete.ok) {
+//         alert('Un problème est intervenu, merci de revenir plus tard.');
+//       } else {
+//         const reponse = await requete.json();
+//         console.log(reponse);
+//       }
+// }
+
+// Effectuer('John');
+
+const axiosInstance = axios.create({
+    baseURL: 'https://lesoublisdelinfo.com',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+});
+
+axiosInstance.post('api.php', new URLSearchParams({
+    prenom: 'John'
+}))
+    .then(function (response) {
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
